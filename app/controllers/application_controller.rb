09d9df7124288546bef4449c88f512be07aa51be
Calvin Lee <lwd369@gmail.com>
2017-02-23 18:43:12 +0800

    add qiniu gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def uptoken
+    put_policy = Qiniu::Auth::PutPolicy.new(
+        'lwd369-bucket', # 存储空间
+        nil,    # 指定上传的资源名，如果传入 nil，就表示不指定资源名，将使用默认的资源名
+        3600    # token 过期时间，默认为 3600 秒，即 1 小时
+    )
+    # 生成上传 Token
+    uptoken = Qiniu::Auth.generate_uptoken(put_policy)
+  end
 end
---
+++ config/initializers/qiniu_sdk.rb
@@ -0,0 +1,4 @@
+require 'qiniu'
+
+Qiniu.establish_connection! access_key: 'RWSJ8YJ190SlCEz2wmV2L1s782Ur7u9NHu2PjTag',
+                            secret_key: '_soxFs-MeB_oKsY7QHgEWZRwxgD33piTHxQFfJWp'
