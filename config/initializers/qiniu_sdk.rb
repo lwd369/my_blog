db301163e4efcbb470e8e51e60b3b6b67c2b880d
Calvin Lee <lwd369@gmail.com>
2017-02-27 11:18:43 +0800

    update qiniu key secrets

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -1,4 +1,4 @@
 require 'qiniu'
 
-Qiniu.establish_connection! access_key: 'RWSJ8YJ190SlCEz2wmV2L1s782Ur7u9NHu2PjTag',
-                            secret_key: '_soxFs-MeB_oKsY7QHgEWZRwxgD33piTHxQFfJWp'
+Qiniu.establish_connection! access_key: Rails.application.secrets.qiniu_access_key,
+                            secret_key: Rails.application.secrets.qiniu_secret_key
